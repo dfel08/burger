8c424f07d2d5b35d2ae48bf093e5d491caadcf21
dfel08 <dfellows68@gmail.com>
2020-04-24 09:53:02 -0700

    Add db and modules

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double-double", FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Animal Style", FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Mushroom Swiss Burger", FALSE);
