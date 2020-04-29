0bf3ad383392fc84ce103c205ec3a27acb154b35
dfel08 <dfellows68@gmail.com>
2020-04-29 09:22:45 -0700

    Add new connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
+    Host: "b4e9xxkxnpu2v96i.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",	
+    Username: "rb07d5zqrmi0ivlg",	
+    Password: "g3k0qw2wvf8xo02i",
+    Port: 3306,
+    Database: "q6tykcl2ht3trqx8"	
 });
 
 //var connection = mysql.createConnection(process.env.JAWSDB_URL);
