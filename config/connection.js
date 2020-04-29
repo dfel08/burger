62f9dbccf868f294b806ecea0d1c8af53278a4cb
dfel08 <dfellows68@gmail.com>
2020-04-29 09:33:27 -0700

    Fix config

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,7 @@
+require("dotenv").config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    Host: "b4e9xxkxnpu2v96i.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",	
-    Username: "rb07d5zqrmi0ivlg",	
-    Password: "g3k0qw2wvf8xo02i",
-    Port: 3306,
-    Database: "q6tykcl2ht3trqx8"	
-});
-
-//var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
     if (err) {
